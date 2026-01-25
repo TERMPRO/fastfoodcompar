5aab8f30e04e7f6d68c08c393fdb1c4134f530d3
TERMPRO <Terminator10550@gmail.com>
2026-01-25 09:27:09 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+const produits = [
+  {
+    fastfood: "McDonald's",
+    nom: "Big Mac",
+    prix: 5.20,
+    calories: 503
+  },
+  {
+    fastfood: "Burger King",
+    nom: "Whopper",
+    prix: 5.90,
+    calories: 657
+  },
+  {
+    fastfood: "KFC",
+    nom: "Zinger",
+    prix: 6.10,
+    calories: 450
+  }
+
+  // ➕ ajoute autant de produits que tu veux
+];
